0e96b1841c250ebbb46e404944a2172ee8c4ce8f
juniormartinez1020 <juniormperalta33@gmail.com>
2024-08-02 15:22:42 -0400

    agregando lista plana de imagenes

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
+import { photos } from './data';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>build the IOPhoto ✔👀🎞📸</Text>
+
+      <FlatList
+      data={photos}
+      numColumns={5}
+      contentContainerStyle={{ gap: 2 }}
+      columnWrapperStyle={{ gap: 2 }}
+      renderItem={({ item }) => 
+      <Image 
+      source={item.image} 
+      style={{ width: `${100 / 5}%`, aspectRatio: 1 }}
+      />
+    } 
+      />
+       
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +29,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
